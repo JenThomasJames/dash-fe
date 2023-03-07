ff939c9952e6f65674827bc728999905331b97dd
JenThomasJames <jenthomasjames123@gmail.com>
2023-03-07 18:49:04 +0530

    dep: Add Important Project Dependencies Added the following dependencies: * TailwindCSS *Material UI *Roboto Font *Material UI Icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
@@ -7,7 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
